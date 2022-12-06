645314fd6d161380b1ce81929249aeaca2f5658f
Conor Sullivan <conorwsullivan@gmail.com>
2022-12-07 02:24:51 +0700

    3D mesh import!

--- src/draw/render.rs
+++ src/draw/render.rs
@@ -1,5 +1,5 @@
 
-use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::{prelude::*, render::camera::ScalingMode, gltf::Gltf};
 
 pub fn render_world() {
 }
@@ -9,58 +9,68 @@ pub fn setup_render(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    ass: Res<AssetServer>,
 ) {
-    // camera
-    commands.spawn(Camera3dBundle {
-        projection: OrthographicProjection {
-            scale: 3.0,
-            scaling_mode: ScalingMode::FixedVertical(2.0),
-            ..default()
-        }
-        .into(),
-        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+  // camera
+  commands.spawn(Camera3dBundle {
+    projection: OrthographicProjection {
+      scale: 3.0,
+      scaling_mode: ScalingMode::FixedVertical(2.0),
+      ..default()
+    }
+    .into(),
+    transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ..default()
+  });
 
-    // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
+  // plane
+  /*commands.spawn(PbrBundle {
+    mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+    material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    ..default()
+  });*/
 
-    // cubes
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(1.5, 0.5, 1.5),
-        ..default()
-    });
+  // cubes
+  commands.spawn(PbrBundle {
+    mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+    transform: Transform::from_xyz(1.5, 0.5, 1.5),
+    ..default()
+  });
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(1.5, 0.5, -1.5),
-        ..default()
-    });
+  commands.spawn(PbrBundle {
+    mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+    transform: Transform::from_xyz(1.5, 0.5, -1.5),
+    ..default()
+  });
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(-1.5, 0.5, 1.5),
-        ..default()
-    });
+  commands.spawn(PbrBundle {
+    mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+    transform: Transform::from_xyz(-1.5, 0.5, 1.5),
+    ..default()
+  });
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(-1.5, 0.5, -1.5),
-        ..default()
-    });
-    
-    // light
-    commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(3.0, 8.0, 5.0),
-        ..default()
-    });
+  /*commands.spawn(PbrBundle {
+    mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+    material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+    transform: Transform::from_xyz(-1.5, 0.5, -1.5),
+    ..default()
+  });*/
+
+  // light
+  commands.spawn(PointLightBundle {
+    transform: Transform::from_xyz(3.0, 8.0, 5.0),
+    ..default()
+  });
+
+  //let my_gltf = ass.load("model/r1000.glb");
+  let my_gltf = ass.load("model/r1000.gltf#Scene0");
+  commands.spawn(SceneBundle {
+      scene: my_gltf,
+      transform: Transform::from_xyz(0.0, 0.5, 0.0),
+      ..Default::default()
+  });
+
 }
