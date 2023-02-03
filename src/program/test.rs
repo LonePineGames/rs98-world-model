2d60e2ce9f41afa94233368f4d9251e27e87ade8
Conor Sullivan <conorwsullivan@gmail.com>
2023-02-04 01:14:35 +0700

    Simplify program tests

--- src/program/test.rs
+++ src/program/test.rs
@@ -5,9 +5,10 @@ use crate::{model::{world::World, auto::{AutoNdx, Auto}, act::Action, kind::Kind
 
 fn run100(world: &mut World, program: &mut ProgramSpace, robo: AutoNdx, expected_steps: usize) {
   let mut steps = 0;
-  while steps < 100 && !program.idle(robo) {
+  while steps < 100 && (!program.idle(robo) || world.get_auto(robo).action != Action::Stop) {
     program.update(1.0);
     program.process_events(world);
+    program.process_messages(world);
     world.update(1.0);
     assert_eq!(world.stall_message(robo), None);
     steps += 1;
@@ -33,23 +34,11 @@ fn test_program_move() {
   });
 
   let mut program = ProgramSpace::new(robo);
+
   program.set_program(robo, p("(goto 20 20)"));
-
-  let mut steps = 0;
-  while steps < 100 {
-    program.update(1.0);
-    program.process_events(&mut world);
-    world.update(1.0);
-    assert_eq!(world.stall_message(robo), None);
-    steps += 1;
-    if world.get_auto(robo).loc == end && world.get_auto(robo).action == Action::Stop {
-      break;
-    }
-  }
-
+  run100(&mut world, &mut program, robo, 21);
   assert_eq!(world.get_auto(robo).loc, end);
   assert_eq!(world.get_auto(robo).action, Action::Stop);
-  assert_eq!(steps, 21);
 }
 
 #[test]
@@ -71,57 +60,21 @@ fn test_input() {
   });
 
   let mut program = ProgramSpace::new(robo);
+
   program.interrupt(robo, p("(input-key A)"));
-  
-  let mut steps = 0;
-  while steps < 100 {
-    program.update( 1.0);
-    program.process_events(&mut world);
-    world.update(1.0);
-    assert_eq!(world.stall_message(robo), None);
-    steps += 1;
-    if world.get_auto(robo).loc == end && world.get_auto(robo).action == Action::Stop && program.idle(robo) {
-      break;
-    }
-  }
-  
+  run100(&mut world, &mut program, robo, 1);
   assert_eq!(world.get_auto(robo).loc, end);
   assert_eq!(world.get_auto(robo).action, Action::Stop);
-  assert_eq!(steps, 1);
 
   program.interrupt(robo, p("(input-key E)"));
-  
-  while steps < 100 {
-    program.update( 1.0);
-    program.process_events(&mut world);
-    world.update(1.0);
-    assert_eq!(world.stall_message(robo), None);
-    steps += 1;
-    if world.get_auto(robo).loc == start && world.get_auto(robo).action == Action::Stop && program.idle(robo) {
-      break;
-    }
-  }
-  
+  run100(&mut world, &mut program, robo, 1);
   assert_eq!(world.get_auto(robo).loc, start);
   assert_eq!(world.get_auto(robo).action, Action::Stop);
-  assert_eq!(steps, 2);
   
   program.interrupt(robo, p("(input-mouse 20 20)"));
-  
-  while steps < 100 {
-    program.update( 1.0);
-    program.process_events(&mut world);
-    world.update(1.0);
-    assert_eq!(world.stall_message(robo), None);
-    steps += 1;
-    if world.get_auto(robo).loc == elsewhere && world.get_auto(robo).action == Action::Stop && program.idle(robo) {
-      break;
-    }
-  }
-  
+  run100(&mut world, &mut program, robo, 21);
   assert_eq!(world.get_auto(robo).loc, elsewhere);
   assert_eq!(world.get_auto(robo).action, Action::Stop);
-  assert_eq!(steps, 23);
 }
 
 #[test]
@@ -145,40 +98,17 @@ fn test_input_pick_place() {
   });
 
   let mut program = ProgramSpace::new(robo);
+  
   program.interrupt(robo, p("(input-key Period)"));
-  
-  let mut steps = 0;
-  while steps < 100 && !program.idle(robo) {
-    program.update( 1.0);
-    program.process_events(&mut world);
-    program.process_messages(&mut world);
-    world.update(1.0);
-    assert_eq!(world.stall_message(robo), None);
-    steps += 1;
-  }
-  
+  run100(&mut world, &mut program, robo, 2);
   assert_eq!(world.get_item(robo, hand), rock);
   assert_eq!(world.get_item(space, start), Kind(0));
   assert_eq!(world.get_auto(robo).action, Action::Stop);
-  assert_eq!(steps, 2);
 
   program.interrupt(robo, p("(input-key Period)"));
-  
-  while steps < 100 {
-    program.update( 1.0);
-    program.process_events(&mut world);
-    program.process_messages(&mut world);
-    world.update(1.0);
-    assert_eq!(world.stall_message(robo), None);
-    steps += 1;
-    if world.get_item(robo, hand) == Kind(0) && world.get_auto(robo).action == Action::Stop && program.idle(robo) {
-      break;
-    }
-  }
-  
+  run100(&mut world, &mut program, robo, 2);
   assert_eq!(world.get_item(robo, hand), Kind(0));
   assert_eq!(world.get_auto(robo).loc, start);
   assert_eq!(world.get_auto(robo).action, Action::Stop);
   assert_eq!(world.get_item(space, start), rock);
-  assert_eq!(steps, 4);
 }
